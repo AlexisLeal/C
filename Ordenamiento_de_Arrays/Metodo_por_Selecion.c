363776b7b0475318039ee697047f84e412539b52
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-23 02:08:36 -0500

    Create Metodo_por_Selecion.c

---
+++ Ordenamiento_de_Arrays/Metodo_por_Selecion.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(void) {
+  int lista[5]={2,5,4,8,6};
+  int i,j,aux,min;
+
+  for(i=0;i<5;i++){
+    min=i;
+    for(j=i+1;j<=5;j++){ //hasta un elemento despues de nuestro array para amarrar, es i mas 1 para que cheque todos realmente 
+      //el i mas uno es para empezar realmenrte de la posicion actual de i y no ver atras 
+      //por que si lo ve atras lo volvera a cambiar y todo queda como empezo TENER CUIDADO 
+      if(lista[j]<lista[min]){
+        min=j;
+      }
+    }
+    aux=lista[i];
+    lista[i]=lista[min];
+    lista[min]=aux;
+  }
+
+
+
+ for(i=0;i<5;i++){
+    printf("%i ",lista[i]);
+  }
+
+
+
+
+
+  return 0;
+}
